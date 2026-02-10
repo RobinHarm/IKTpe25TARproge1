9919a5b9eb8ec509cb47f80512057a153b025bab
Robinarm <robinpoiss99@gmail.com>
2026-02-10 15:54:08 +0200

    "andmebaas"
    LISTIGA andmebaas

---
+++ List link/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+{ 
+ public class Person
+ {
+ 	public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+ 	public int age { get; set; }
+	
+ }
+}
---
+++ List link/List link/Program.cs
@@ -0,0 +1,68 @@
+﻿using List_link;
+
+namespace ListLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List and LINQ");
+
+          //teeme "andmebaasi"
+          //ja selleks on vaja luua class nimega Person
+          //mis on muutuja all nimega person
+            IList<Person> person = new List<Person>()
+            {
+             new Person() {Id = 1, Name = "Juku", Age = 10},
+             new Person() {Id = 2, Name = "Juhan", Age = 11},
+             new Person() {Id = 3, Name = "Maali", Age = 9},
+             new Person() {Id = 4, Name = "Aksel", Age = 10},
+            };
+
+            //nüüd kastuame person muutujat uue muutuja all
+            //mille nimeks on persons
+            var persons = from p in person
+                          select new
+                          {
+                              Id = p.Id,
+                              Name = p.Name,
+                              Age = p.Age
+                          };
+               //kasutame muutujat persons, et näidata konsoolis tulemust
+               foreach (var item in persons)
+               {
+                Console.WriteLine("Id on " + item.Id + " nimi on " + item.Name + " ja vanus on " + item.Age);
+               }
+
+            Console.WriteLine("___--------------------------");
+            Console.WriteLine("Kasutame LINQ Selecti ehk teine variant");
+            //siin koondame kogu info result muutuja sisse
+            var result = person
+                .Where(p => p.Id == 1 || p.Age == 9) //Where-ga saab teha konkreetse päringu, et vastab mingitele tingimustele.
+                .OrderBy(p => p.Name) //järjestab isikud nime järgi
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Age = x.Age
+                });
+            //kasutame result muutujat  ja teeme ta lahti rea kaupa
+            //läbi muutuja item 
+            foreach (var item in result)
+            {
+                Console.WriteLine("Id on " + item.Id + " nimi on " + item.Name + " ja vanus on " + item.Age);
+            }
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Gruppide kaupa sorteerimine");
+
+            var groupBy = person
+                .GroupBy(p => p.Age);
+
+            foreach (var item in groupBy)
+            {
+                Console.WriteLine("Vanuse grupp on: {0}", item.Key);
+            }
+        }
+    }
+}
+ 

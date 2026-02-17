8fdbe30306568107682290528bfb7d53abd83f8b
Opilane TTHK <robinpoiss99@gmail.com>
2026-02-17 15:53:42 +0200

    LINQ
    fafaaf

---
+++ LINQ/LINQ/PeopleData.cs
@@ -0,0 +1,60 @@
+﻿using LINQ.Models;
+
+namespace LINQ
+{
+    public class PeopleData
+    {
+        public static readonly List<PeopleList> peoples = new List<PeopleList>
+        {
+             new PeopleList()
+             {
+                 Id = 1,
+                 Name = "Moona",
+                 Age = 22,
+                 GenderId = Guid.Parse("e6a57d34-7345-4855-8489-952f0503d7c1")
+             },
+             new PeopleList()
+             {
+                 Id = 2,
+                 Name = "Poona",
+                 Age = 21,
+                 GenderId = Guid.Parse("44772175-8cf4-4b52-aa09-5d412ce4fa9c")
+             },
+             new PeopleList()
+             {
+                 Id = 3,
+                 Name = "Ron",
+                 Age = 18,
+                 GenderId = Guid.Parse("44772175-8cf4-4b52-aa09-5d412ce4fa9c")
+             },
+             new PeopleList()
+             {
+                 Id = 4,
+                 Name = "Anna",
+                 Age = 20,
+                 GenderId = Guid.Parse("e6a57d34-7345-4855-8489-952f0503d7c1")
+             },
+             new PeopleList()
+             {
+                 Id = 5,
+                 Name = "EMEX",
+                 Age = 69,
+                 GenderId = Guid.Parse("44772175-8cf4-4b52-aa09-5d412ce4fa9c")
+             },
+             new PeopleList()
+             {
+                 Id = 6,
+                 Name = "EMEX",
+                 Age = 67,
+                 GenderId = Guid.Parse("44772175-8cf4-4b52-aa09-5d412ce4fa9c")
+             },
+             new PeopleList()
+             {
+                 Id = 7,
+                 Name = "Bill",
+                 Age = 69,
+                 GenderId = Guid.Parse("44772175-8cf4-4b52-aa09-5d412ce4fa9c")
+             },
+        };   
+    }
+}
---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kutsume esile LINQ läbi switchi");
+            //Console.WriteLine("Vali vastav link numriga");
+            //Console.WriteLine("1. Where");
+
+            //int choice = int.Parse(Console.ReadLine());
+            WhereBynameLINQ();
+
+        //    switch (choice)
+        //    {
+        //        case 1:
+        //            WhereLINQ();
+        //            break;
+
+        //        default:
+        //            break;
+        //    }
+        //    WhereLINQ();
+        }
+
+
+        //teeme uue meetodid
+        //public static void WhereLINQ()
+        //{
+
+        //    var peopleAge = PeopleData.peoples
+        //        .Where(x => x.Age > 20 && x.Age < 23);
+
+        //    foreach (var people in peopleAge)
+        //    {
+        //        Console.WriteLine(people.Name);
+        //    }
+
+
+        //    //kasutada muutujat peopleAge ja kuvada andmed esile
+        //    //kasuta foreachi
+        //}
+
+        public static void WhereBynameLINQ()
+        {
+            Console.WriteLine("Kirjuta inimese nimi: ");
+            string name = Console.ReadLine();
+            var peopleName = PeopleData.peoples
+                .Where(x => x.Name == name);
+
+            foreach (var people in peopleName)
+            {
+                Console.WriteLine(people.Name + people.Age);
+            }
+        }
+           
+    }
+            
+    
+
+
+}
+
+
+
+
+
+
+
+
